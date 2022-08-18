c0e36568eed9a73140eb6a23aa4d14e4bedcd3bb
Davidzambrano16 <david-z-p@hotmail.com>
2022-08-18 13:19:41 -0500

    ultimo commit

--- assets/main.js
+++ assets/main.js
@@ -1,8 +1,19 @@
 let menu = document.querySelector("ul.nav--menu")
 let icon = document.getElementById("icon-menu")
-
+let botonHome = document.getElementById("botonHome")
+let botonAcerca = document.getElementById("botonAcerca")
+let botonHabilidad = document.getElementById("botonHabilidad")
+let botonExperiencia = document.getElementById("botonExperiencia")
+let botonPortafolio = document.getElementById("botonPortafolio")
+let botonContacto = document.getElementById("botonContacto")
 
 icon.addEventListener("click", toggleMenu)
+botonHome.addEventListener("click", toggleMenu)
+botonAcerca.addEventListener("click", toggleMenu)
+botonHabilidad.addEventListener("click", toggleMenu)
+botonExperiencia.addEventListener("click", toggleMenu)
+botonPortafolio.addEventListener("click", toggleMenu)
+botonContacto.addEventListener("click", toggleMenu)
 
 function toggleMenu(){
     menu.classList.toggle("visible")
